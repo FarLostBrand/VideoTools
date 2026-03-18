d185f2a1b21632339081013a2f9a8ae82654a8ef
FarLostBrand <farlostbrand@gmail.com>
2026-03-18 17:07:38 -0400

    v1.1.1 Updated versioning.

--- src/App.tsx
+++ src/App.tsx
@@ -79,7 +79,7 @@ function AppInner() {
           ))}
         </nav>
         <div className="sidebar-footer">
-          <span className="version">v1.1.0</span>
+          <span className="version">v1.1.1</span>
         </div>
       </aside>
 
