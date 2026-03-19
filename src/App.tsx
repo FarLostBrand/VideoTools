ca0f4b907f4c0f0bffc03d16ebabecdd291aa141
FarLostBrand <farlostbrand@gmail.com>
2026-03-19 17:00:26 -0400

    v1.2.1 New Icon!

--- src/App.tsx
+++ src/App.tsx
@@ -79,7 +79,7 @@ function AppInner() {
           ))}
         </nav>
         <div className="sidebar-footer">
-          <span className="version">v1.2.0</span>
+          <span className="version">v1.2.1</span>
         </div>
       </aside>
 
